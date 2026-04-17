0841ae5e3d6721818dfa03b9bda713932c2d0d17
Michael Arsenault <mdarsenaultei@gmail.com>
2026-04-17 14:58:43 +0800

    Update health.service.spec.ts

--- src/health/services/health.service.spec.ts
+++ src/health/services/health.service.spec.ts
@@ -1,14 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { LoggerService } from '@/common/logger/logger.service';
+
 import { HealthService } from './health.service';
+
+import { mockLoggerService } from '@/test-utils/mocks/logger.service.mock';
 
 describe('HealthService', () => {
   let service: HealthService;
 
+  let childLogger: any;
+
   beforeEach(async () => {
+    jest.clearAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HealthService],
+      providers: [
+        HealthService,
+        {
+          provide: LoggerService,
+          useValue: mockLoggerService,
+        },
+      ],
     }).compile();
+
+    childLogger = mockLoggerService.child('HealthService');
 
     service = module.get<HealthService>(HealthService);
   });
@@ -22,6 +38,8 @@ describe('HealthService', () => {
       const result = await service.checkHealth();
 
       expect(result).toEqual({ status: 'ok' });
+
+      expect(childLogger.info).toHaveBeenCalledWith('Health check successful');
     });
   });
 });
